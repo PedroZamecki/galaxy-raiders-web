2cc44b9147b004e4a63afa2ae931e2913050714c
Pedro Zamecki <pedrozamecki@gmail.com>
2023-07-22 21:08:59 -0300

    Made new pages to Game and LeaderBoard

---
+++ pages/LeaderBoard.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="background">
+        <h1 class="header">
+            Leader Board
+        </h1>
+        <div class="leaderBoard">
+            <div class="lbItem lbHeaders">
+                <span>Name</span>
+                <span>Score</span>
+                <span>Time</span>
+            </div>
+            <div class="lbItem playerItem" v-for="player in players_mock" :key="player.id">
+                <span>{{ player.name }}</span>
+                <span>{{ player.score }}</span>
+                <span>{{ player.time }}</span>
+            </div>
+        </div>
+        <div class="buttons">
+            <button class="button" @click="$router.push(`/`)">Back</button>
+        </div>
+    </div>
+</template>
+
+<style>
+.header {
+    font-size: 5rem;
+    text-align: center;
+    animation: rainbow 4s linear infinite;
+    font-family: 'Courier New', Courier, monospace;
+}
+
+.buttons {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+
+.button {
+    margin: 1rem;
+    padding: 1rem;
+    font-size: 2rem;
+    cursor: pointer;
+    color: #f5f5f5;
+    border-radius: 1rem;
+    background-color: #36bbf5;
+    border: 0.2rem solid #36bbf5;
+    transition: all 0.2s ease-in-out;
+    font-family: 'Courier New', Courier, monospace;
+}
+
+.button:hover {
+    color: #36bbf5;
+    background-color: #f5f5f5;
+    border: 0.2rem solid #f5f5f5;
+}
+
+.leaderBoard {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.lbHeaders {
+    color: #36bbf5;
+    background-color: #f5f5f5;
+    border: 0.2rem solid #f5f5f5;
+}
+
+.lbItem {
+    width: 150%;
+    height: 100%;
+    margin: 1rem;
+    display: flex;
+    padding: 1rem;
+    font-size: medium;
+    border-radius: 1rem;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    transition: all 0.2s ease-in-out;
+    font-family: 'Courier New', Courier, monospace;
+}
+
+.playerItem {
+    color: #f5f5f5;
+    background-color: #36bbf5;
+    border: 0.2rem solid #36bbf5;
+}
+</style>
+
+<script setup>
+const {
+    data: players,
+} = await $get("/players");
+
+// Mock Values!
+const players_mock = [
+    {
+        name: "Player 1",
+        score: 100,
+        time: 100
+    },
+    {
+        name: "Player 2",
+        score: 50,
+        time: 50
+    },
+    {
+        name: "Player 3",
+        score: 10,
+        time: 10
+    }
+]
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,115 +1,87 @@
 <template>
-  <div id="canvas">
-    <div id="deep-space" />
-    <div id="space-field">
-      <SpaceObject id="spaceship" class="spaceship" :data="spaceField.ship" resolution="2" />
-
-      <SpaceObject class="asteroid" :data="asteroid" resolution="2"
-                  :key="asteroid.center"
-                  v-for="asteroid in spaceField.asteroids" />
-
-      <SpaceObject class="missile" :data="missile" resolution="2"
-                  :key="missile.center"
-                  v-for="missile in spaceField.missiles" />
-
-      <SpaceObject class="explosion" :data="explosion" resolution="2"
-                  :key="explosion.center"
-                  v-for="explosion in spaceField.explosions" />
-    </div>
-  </div>
+	<div class="background">
+		<h1 class="header">Menu</h1>
+		<div class="buttons">
+			<button class="button" @click="$router.push(`/Game`)">Start</button>
+			<button class="button" @click="$router.push(`/LeaderBoard`)">Leader Board</button>
+			<button class="button" @click="exit()">Exit</button>
+		</div>
+	</div>
 </template>
 
 <script setup>
-const {
-  data: spaceField,
-  refresh: updateSpaceField
-} = await $get("/space-field");
-
-onMounted(() => {
-  window.addEventListener("keydown", async (event) => {
-    const keyToCommand = {
-      "ArrowUp": "MOVE_SHIP_UP",
-      "ArrowDown": "MOVE_SHIP_DOWN",
-      "ArrowRight": "MOVE_SHIP_RIGHT",
-      "ArrowLeft": "MOVE_SHIP_LEFT",
-      "Space": "LAUNCH_MISSILE",
-      "Escape": "PAUSE_GAME",
-    };
-
-    const command = keyToCommand[event.code];
-
-    // Ignore if invalid key was pressed
-    if (command === undefined) return;
-
-    console.log(`Triggering command: ${command}`);
-    await $post("/ship/commands", { command })
-  });
-
-  window.setInterval(updateSpaceField, 1000);
-})
+async function exit() {
+	await $post("/exit");
+	// Não funcionando? Culpa do servidor?
+} 
 </script>
 
 <style>
-#canvas {
-  height: calc(100vh - 4rem);
-  width: calc(100vw - 4rem);
+@keyframes rainbow {
+	0% {
+		color: #f5f5f5;
+	}
 
-  padding: 2rem;
+	25% {
+		color: #36bbf5;
+	}
 
-  background-color: #36bbf5;
-  overflow: hidden;
+	50% {
+		color: #00ffaa;
+	}
 
-  position: relative;
+	75% {
+		color: #c300ff;
+	}
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	100% {
+		color: #f5f5f5;
+	}
 }
 
-@keyframes slide {
-  0% {
-    transform: translate(1px);
-  }
-  50% {
-    transform: translate(-1px);
-  }
-  100% {
-    transform: translate(1px);
-  }
+.background {
+	width: 100vw;
+	height: 100vh;
+
+	background-origin: content-box;
+	background-image: url("~/assets/space.png");
+
+	z-index: 0;
+	overflow: hidden;
+	position: relative;
+
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	justify-content: center;
 }
 
-#deep-space {
-  height: calc(100% - 4rem);
-  width: calc(100% - 4rem);
-
-  background-image: url("~/assets/space.png");
-  background-origin: content-box;
-  animation: slide 3s linear infinite;
-
-  position: absolute;
-  z-index: 0;
+.header {
+	text-align: center;
+	font-size: xxxxxx-large;
+	animation: rainbow 4s linear infinite;
+	font-family: 'Courier New', Courier, monospace;
 }
 
-#space-field {
-  height: calc(100% - 4rem);
-  width: calc(100% - 4rem);
-
-  position: relative;
+.buttons {
+	display: flex;
+	align-items: center;
+	flex-direction: column;
 }
 
-.spaceship {
-  background-image: url("~/assets/spaceship.png");
+.button {
+	width: 12rem;
+	height: 5rem;
+	margin: 0.5rem;
+	color: #36bbf5;
+	font-size: medium;
+	border-radius: 1rem;
+	background: #f5f5f5;
+	font-family: 'Courier New', Courier, monospace;
 }
 
-.asteroid {
-  background-image: url("~/assets/asteroid.png");
-}
-
-.missile {
-  background-image: url("~/assets/missile.png");
-}
-
-.explosion {
-  background-image: url("~/assets/explosion.png");
-}
-</style>
+.button:hover {
+	color: #f5f5f5;
+	background-color: #36bbf5;
+	border: 0.2rem solid #36bbf5;
+}</style>
